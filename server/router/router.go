f67778d5eaa7d0409d2dc69d890251caf3ed46cd
hyperxpizza <wojtek.frackowski@gmail.com>
2021-07-22 13:41:19 +0200

    add websocket endpoint

--- server/router/router.go
+++ server/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"fmt"
+	"linker-fan/gal-anonim-server/server/handlers"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +14,13 @@ func Run(port string, mode string) {
 
 func setupRoutes() *gin.Engine {
 	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	chat := r.Group("/chat")
+	{
+		chat.GET("/ws", handlers.ChatWebsocket)
+	}
 
 	return r
 }
