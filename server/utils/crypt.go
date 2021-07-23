acfff6eb621b497aeccf940ababc1834be319e40
hyperxpizza <wojtek.frackowski@gmail.com>
2021-07-23 16:11:47 +0200

    add password hashing functions

---
+++ server/utils/crypt.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	return string(hashedPassword), nil
+}
+
+func CompareHashAndPassword(hashedPassword, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
