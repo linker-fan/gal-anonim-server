2fced10c1527c8e48d0cc3828c05ee541ca392c9
hyperxpizza <wojtek.frackowski@gmail.com>
2021-07-23 17:10:44 +0200

    register handler and users database functions

--- server/database/users.go
+++ server/database/users.go
@@ -1,10 +1,14 @@
 package database
 
 import (
+	"database/sql"
+	"errors"
 	"log"
 	"time"
 )
 
+//InsertUser takes username and hashed password from the register handler and creates a new row in users table. by default new user is not an admin
+//@author hyperxpizza
 func InsertUser(username, passwordHash string) error {
 	stmt, err := db.Prepare("insert into users(id,username,passwordHash,isAdmin,created,updated) values (default, $1, $2, $3, $4, $5)")
 	if err != nil {
@@ -19,4 +23,20 @@ func InsertUser(username, passwordHash string) error {
 	}
 
 	return nil
+}
+
+//CheckIfUsernameExists queries the users table to check if row with username given as an argument to the function already exists in the database
+//@author hyperxpizza
+func CheckIfUsernameExists(username string) error {
+	var u string
+	err := db.QueryRow("select username from users where username=$1", username).Scan(&u)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil
+		} else {
+			return err
+		}
+	}
+
+	return errors.New("Username already taken")
 }
--- server/handlers/users.go
+++ server/handlers/users.go
@@ -1,8 +1,76 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"linker-fan/gal-anonim-server/server/database"
+	"linker-fan/gal-anonim-server/server/utils"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+type RegisterRequest struct {
+	Username  string `json:"username"`
+	Password1 string `json:"password1"`
+	Password2 string `json:"password2"`
+}
+
+//Register handler validates json and inserts new user into the database
+//@author hyperxpizza
 func Register(c *gin.Context) {
+	var request RegisterRequest
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.Status(http.StatusBadRequest)
+		return
+	}
+
+	//validate input data
+	err := utils.ValidateUsername(request.Username)
+	if err != nil {
+		c.Status(http.StatusNotAcceptable)
+		return
+	}
+
+	//check if username is already taken
+	err = database.CheckIfUsernameExists(request.Username)
+	if err != nil {
+		if err.Error() == "Username already taken" {
+			c.Status(http.StatusConflict)
+			return
+		} else {
+			c.Status(http.StatusInternalServerError)
+			return
+		}
+	}
+
+	//check if passwords are the same
+	if request.Password1 != request.Password2 {
+		c.Status(http.StatusNotAcceptable)
+		return
+	}
+
+	//validate password
+	err = utils.ValidatePassword(request.Password1)
+	if err != nil {
+		c.Status(http.StatusNotAcceptable)
+		return
+	}
+
+	//generate password hash
+	passwordHash, err := utils.HashPassword(request.Password1)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	err = database.InsertUser(request.Username, passwordHash)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	c.Status(http.StatusCreated)
+	return
 
 }
 
