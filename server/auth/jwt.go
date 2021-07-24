98ef345c1b9b42522a009698462bd2d14931ce11
hyperxpizza <wojtek.frackowski@gmail.com>
2021-07-24 10:02:06 +0200

    isTokenValid function

--- server/auth/jwt.go
+++ server/auth/jwt.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"errors"
+	"fmt"
 	"linker-fan/gal-anonim-server/server/config"
 	"log"
 	"time"
@@ -47,4 +49,29 @@ func GenerateJWTToken(username string, id int, isAdmin bool) (string, *time.Time
 	}
 
 	return tokenString, &expirationTime, nil
+}
+
+func IsTokenValid(tokenString string) (string, int, bool, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok == false {
+			return nil, fmt.Errorf("Token signing method is not valid: %v", token.Header["alg"])
+		}
+
+		return []byte(c.Jwt.TokenSecret), nil
+	})
+
+	if err != nil {
+		log.Println(err)
+		return "", 0, false, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		idFloat := claims["id"]
+		id := int(idFloat.(float64))
+		username := claims["username"]
+		isAdmin := claims["is_admin"]
+		return username.(string), id, isAdmin.(bool), nil
+	}
+
+	return "", 0, false, errors.New("Reading token claims failed")
 }
