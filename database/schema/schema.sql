fcc610d58e4803816efcec5963a61b6b562431e9
hyperxpizza <wojtek.frackowski@gmail.com>
2021-07-24 13:28:29 +0200

    add room tables to sql schema

--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -2,18 +2,20 @@ create table users (
 	id serial primary key,
 	username varchar(30) unique not null,
 	passwordHash text not null,
-        isAdmin boolean not null, 
+    isAdmin boolean not null, 
 	created timestamp not null,
 	updated timestamp not null
 );
 
--- create table rooms (
---	id text primary key not null,
---	ownerID integer not null references users(id) on delete set null
---);
+create table room_members (
+	id serial primary key,
+	userID integer references users(id) on delete set null
+); 
 
---create table room_members (
---	id serial primary key,
---	roomID integer not null references rooms(id) on delete cascade,
---	userID integer references users(id) on delete set null,
---); 
+create table rooms (
+	id text primary key not null,
+	roomName varchar(50) not null,
+	roomPasswordHash text not null, 
+	ownerID integer not null references users(id) on delete set null,
+	roomMembersID integer not null references room_members(id) on delete set null
+);
