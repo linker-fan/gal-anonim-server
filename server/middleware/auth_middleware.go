635acd84a7308c4f4478f243a6022bbf68e66a36
hyperxpizza <wojtek.frackowski@gmail.com>
2021-07-24 12:13:25 +0200

    auth middleware

--- server/middleware/auth_middleware.go
+++ server/middleware/auth_middleware.go
@@ -1,21 +1,39 @@
 package middleware
 
 import (
+	"linker-fan/gal-anonim-server/server/auth"
+	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 //JwtMiddleware - not sure wether to use cookies or return tokenstrings
 func JwtMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		/*
-			cookie, err := c.Request.Cookie("jwtToken")
-			if err != nil {
-				c.Status(http.StatusUnauthorized)
-				c.Abort()
-				return
-			}
 
-			tokenString := cookie.Value
-		*/
+		cookie, err := c.Request.Cookie("jwtToken")
+		if err != nil {
+			c.Status(http.StatusUnauthorized)
+			c.Abort()
+			return
+		}
+
+		tokenString := cookie.Value
+		username, id, isAdmin, err := auth.IsTokenValid(tokenString)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"message": "Not Authorized",
+			})
+			c.Abort()
+			return
+		} else {
+			c.Set("id", id)
+			c.Set("username", username)
+			c.Set("is_admin", isAdmin)
+			c.Next()
+		}
+
 	}
 }
