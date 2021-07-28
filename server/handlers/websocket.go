4a50ba733bcc8329b92610251932e7de23625057
hyperxpizza <wojtek.frackowski@gmail.com>
2021-07-28 14:40:53 +0200

    read/write functions

--- server/handlers/websocket.go
+++ server/handlers/websocket.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"bytes"
 	"linker-fan/gal-anonim-server/server/database"
+	"log"
 	"net/http"
 	"time"
 
@@ -80,9 +82,69 @@ type Client struct {
 	send chan []byte
 }
 
-func (c *Client) read() {}
+func (c *Client) read() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
 
-func (c *Client) write() {}
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		c.hub.broadcast <- message
+	}
+}
+
+func (c *Client) write() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if !ok {
+				// The hub closed the channel.
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(message)
+
+			// Add queued chat messages to the current websocket message.
+			n := len(c.send)
+			for i := 0; i < n; i++ {
+				w.Write(newline)
+				w.Write(<-c.send)
+			}
+
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
 
 func ChatWebsocket(c *gin.Context) {
 
@@ -111,6 +173,8 @@ func ChatWebsocket(c *gin.Context) {
 	}
 
 	hub := newChatHub()
+	go hub.run()
+
 	client := &Client{hub: hub, conn: ws, send: make(chan []byte, 256)}
 	client.hub.register <- client
 
