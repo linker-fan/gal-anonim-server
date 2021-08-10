87551cc1906308428af2f2b81e53010dbd3c078e
hyperxpizza <wojtek.frackowski@gmail.com>
2021-08-10 13:37:30 +0200

    room boilerplate

---
+++ server/hub/room.go
@@ -0,0 +1,23 @@
+package hub
+
+type Room struct {
+	name       string
+	clients    map[*Client]bool
+	register   chan *Client
+	unregister chan *Client
+	broadcast  chan *Message
+}
+
+func NewRoom(name string) *Room {
+	return &Room{
+		name:       name,
+		clients:    make(map[*Client]bool),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		broadcast:  make(chan *Message),
+	}
+}
+
+func (r *Room) Run() {
+
+}
