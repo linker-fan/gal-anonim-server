27460acba011983f71e37684942ef4d9d4d0be2a
hyperxpizza <wojtek.frackowski@gmail.com>
2021-08-25 18:06:10 +0200

    add file storage to api handler

--- server/filestorage/filestorage.go
+++ server/filestorage/filestorage.go
@@ -1,6 +1,8 @@
 package filestorage
 
 import (
+	"bytes"
+	"errors"
 	"log"
 
 	"github.com/minio/minio-go"
@@ -20,4 +22,48 @@ func NewFileStorage(endpoint, accessKeyID, secretAccessKey string, secure bool) 
 	return &FileStorage{
 		client: c,
 	}, nil
+}
+
+func (fs *FileStorage) StoreFile(bucketName string, file *bytes.Buffer, filename string) error {
+	bucketExists, err := fs.CheckIfBucketExists(bucketName)
+	if err != nil {
+		return err
+	}
+
+	if !bucketExists {
+		return errors.New("Bucket does not exist")
+	}
+
+	_, err = fs.client.PutObject(bucketName, filename, file, int64(file.Len()), minio.PutObjectOptions{ContentType: "application/octet-stream"})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (fs *FileStorage) CheckIfBucketExists(name string) (bool, error) {
+	exists, err := fs.client.BucketExists(name)
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
+}
+
+func (fs *FileStorage) CreateBucketIfDoesNotExist(name string) error {
+	exists, err := fs.CheckIfBucketExists(name)
+	if err != nil {
+		return err
+	}
+
+	if exists {
+		return nil
+	} else {
+		err := fs.client.MakeBucket(name, "default")
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
--- server/handlers/api_handler.go
+++ server/handlers/api_handler.go
@@ -3,12 +3,14 @@ package handlers
 import (
 	"linker-fan/gal-anonim-server/server/config"
 	"linker-fan/gal-anonim-server/server/database"
+	"linker-fan/gal-anonim-server/server/filestorage"
 	"linker-fan/gal-anonim-server/server/hub"
 )
 
 type API struct {
 	dw       *database.DatabaseWrapper
 	wsServer *hub.Hub
+	fs       *filestorage.FileStorage
 }
 
 func NewAPIWrapper(c *config.Config) (*API, error) {
@@ -22,9 +24,15 @@ func NewAPIWrapper(c *config.Config) (*API, error) {
 		return nil, err
 	}
 
+	fs, err := filestorage.NewFileStorage(c.FileStorage.Endpoint, c.FileStorage.AccessKeyID, c.FileStorage.SecretAccessKey, true)
+	if err != nil {
+		return nil, err
+	}
+
 	api := API{
 		dw:       dw,
 		wsServer: hub,
+		fs:       fs,
 	}
 
 	go api.wsServer.Run()
